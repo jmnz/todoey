5acf0ac0b6553437734dd784e2a713943fbb60e6
jesus jimenez <jesusjimenez@jesuss-iMac.local>
2019-03-09 14:13:15 -0500

    populated TableView Cell with dummy data

--- todoey/TodoListViewController.swift
+++ todoey/TodoListViewController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class TodoListViewController: UITableViewController{ 
+class TodoListViewController: UITableViewController{
+    
+    let itemArray = ["Find Mike", "Buy Eggs", "Destroy Demogorgon"  ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    //MARK: - TableView DataSource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
 
 }
 
