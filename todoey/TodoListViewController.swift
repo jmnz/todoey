cc505c6cf6067f9e62e83d1f1ae3e6b291b5800e
jesus jimenez <jesusjimenez@jesuss-iMac.local>
2019-03-12 21:25:48 -0400

    coded persistent data inside the app for new items in the list

--- todoey/TodoListViewController.swift
+++ todoey/TodoListViewController.swift
@@ -11,10 +11,15 @@ import UIKit
 class TodoListViewController: UITableViewController{
     
     var itemArray = ["Find Mike", "Buy Eggs", "Destroy Demogorgon"  ]
+    
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
     
     //MARK: - TableView DataSource Methods
@@ -52,6 +57,7 @@ class TodoListViewController: UITableViewController{
             print("success")
             print(textField.text!)
             self.itemArray.append(textField.text ?? "")
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
         }
         
